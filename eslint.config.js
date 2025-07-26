537a06443b4a4855f34588b087a21eda1eda340f
NileshSree1072 <nileshsree07@gmail.com>
2025-07-26 22:24:33 +0530

    Fix build errors and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,9 @@
-// eslint.config.js
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
-import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
-  js.configs.recommended,
   {
     plugins: {
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
+      'react-hooks': reactHooks,
     },
     rules: {
       'react-hooks/rules-of-hooks': 'error',
