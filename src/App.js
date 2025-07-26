2f189eabb301d67375da0d904bf855a38d14f433
NileshSree1072 <nileshsree07@gmail.com>
2025-07-26 22:53:59 +0530

    Fix: Set default route to /home

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="App min-h-screen bg-gray-900 text-white">
         <Navbar />
         <Routes>
